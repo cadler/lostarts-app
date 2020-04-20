ffd572fa6652571f595786e0af6c4ec0092712a6
Scott Robbin <scott@robbin.co>
2020-04-20 11:05:19 -0500

    feat: iOS universal linking

--- App.tsx
+++ App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import { StatusBar } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StatusBar, Linking } from 'react-native';
 import { WebView } from 'react-native-webview';
 
+const DEFAULT_URI = 'https://www.lostarts.xyz';
+
 export default function App() {
+  const [uri, setUri] = useState(DEFAULT_URI);
+
+  useEffect(() => {
+    Linking.getInitialURL().then(handleUriChange);
+    Linking.addEventListener('url', ({ url }) => handleUriChange(url));
+  }, []);
+
+  function handleUriChange(val: string) {
+    val.startsWith(DEFAULT_URI) && setUri(val);
+  }
+
   return (
     <>
       <StatusBar hidden={true} />
       <WebView
-        source={{uri: "https://www.lostarts.xyz" }}
+        source={{ uri }}
         style={{ marginTop: 40, marginBottom: 30 }}
       />
     </>
